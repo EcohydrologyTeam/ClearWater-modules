be3cb773f154196e2e964e4c04722ceb700d4215
kewalak <kelsey.n.walak@usace.army.mil>
2022-12-22 14:28:15 -0800

    Finished Phosphorus Test Cases

--- src/clearwater_modules_python/nsm1/_phosphorus.py
+++ src/clearwater_modules_python/nsm1/_phosphorus.py
@@ -118,6 +118,8 @@ class Phosphorus:
                 AbDeath_OrgP = 0.0
 
             dOrgPdt = ApDeath_OrgP + AbDeath_OrgP - OrgP_DIP_decay - OrgP_Settling
+        else: 
+            dOrgPdt = 0
         
         # Total Inorganic Phosphorus  (mg/P/day)
         '''
@@ -155,6 +157,8 @@ class Phosphorus:
                 DIP_AbGrowth = 0.0
 
             dTIPdt = OrgP_DIP_decay - TIP_Settling + DIPfromBed + DIP_ApRespiration - DIP_ApGrowth + DIP_AbRespiration - DIP_AbGrowth 
+        else :
+            dTIPdt = 0
 
         #Derived variable calculations
         TOP = 0.0
