6824ad3e49902410ea1d4e9d1c0bc85e944dbb22
xaviernogueira <xavier.rojas.nogueira@gmail.com>
2023-08-23 13:43:52 -0400

    fixing docstring quotes

--- src/clearwater_modules_python/nsm1/_temp_correction.py
+++ src/clearwater_modules_python/nsm1/_temp_correction.py
@@ -1,4 +1,4 @@
-'''
+"""
 =======================================================================================
 Nutrient Simulation Module 1 (NSM1): Temperature Correction
 =======================================================================================
@@ -19,33 +19,34 @@ developed by:
 Version 1.0
 
 Initial Version: June 5, 2021
-'''
+"""
+
 
 class TempCorrection:
-    '''
+    """
     Temperature correction class
-    '''
+    """
 
     def __init__(self, rc20, theta):
-        '''
+        """
         Initialize temperature correction
 
         rc20 : float
             Kinetics reaction (decay) coefficient at 20 degrees Celsius
         theta : float
             Temperature correction factor
-        '''
+        """
         self.rc20 = rc20
         self.theta = theta
 
-    def arrhenius_correction(self, TwaterC) :
-        '''
+    def arrhenius_correction(self, TwaterC):
+        """
         Computes an adjusted kinetics reaction rate coefficient for the specified water 
         temperature using the van't Hoff form of the Arrhenius equation
 
         Parameters:
             TwaterC (float): Water temperature in degrees Celsius
-        '''
+        """
         return_value = self.rc20 * self.theta**(TwaterC - 20.0)
 
         return return_value
