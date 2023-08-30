9da3e33e08dfe63df6d3ef1dbd3de69593b896f1
xaviernogueira <xavier.rojas.nogueira@gmail.com>
2023-08-30 16:18:17 -0400

    Update base.py WIP

--- src/clearwater_modules_python/base.py
+++ src/clearwater_modules_python/base.py
@@ -2,30 +2,50 @@
 
 import abc
 import xarray as xr
+from dataclasses import dataclass
 from typing import (
     TypedDict,
     Callable,
     Optional,
 )
 
-Equation = Callable[..., float]
+Process = Callable[..., float]
 ConstantsDict = dict[str, float | int | bool]
+
+
+@dataclass(slots=True, frozen=True)
+class Variable:
+    """Variable type."""
+    name: str
+    long_name: str
+    units: str
+    description: str
+    process: Process
 
 
 class Model(abc.ABC):
     """Abstract base class for all processes."""
-    
+
     __required_inputs: TypedDict
     __default_constants: TypedDict
-    __default_equations: list[Equation]
-    
+    __default_variables: list[Variable]
+
     def __init__(
         self,
         required_inputs: ConstantsDict | xr.Dataset,
-        update_constants: Optional[ConstantsDict],
-        update_equations: Optional[list[Equation]],
+        update_constants: Optional[ConstantsDict] = None,
+        track_non_state_variables: bool = False,
+        ignore_variables: Optional[list[Variable | str]] = None,
         **kwargs,
     ) -> None:
+        """Initialize the model.
+
+        Args:
+            required_inputs: Required inputs for the model.
+            update_constants: Optional constants to update.
+            track_non_state_variables: Track non-state variables.
+            ignore_variables: Variables to ignore.
+        """
         ...
 
     @abc.abstractmethod
@@ -34,12 +54,11 @@ class Model(abc.ABC):
         ...
 
     @abc.abstractproperty
-    def constants(self) -> ConstantsDict| xr.Dataset:
+    def constants(self) -> ConstantsDict | xr.Dataset:
         """Return the constants."""
-        ... 
-
-    @abc.abstractmethod
-    def run(self) -> ConstantsDict | xr.Dataset:
-        """Run the process."""
         ...
 
+    @abc.abstractmethod
+    def run(self) -> xr.Dataset:
+        """Run the process."""
+        ...
