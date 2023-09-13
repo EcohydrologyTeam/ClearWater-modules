240671e48b8c846cc28b214689c006e5f973d537
xaviernogueira <xavier.rojas.nogueira@gmail.com>
2023-09-13 12:30:29 -0400

    Build model run

--- src/clearwater_modules_python/base.py
+++ src/clearwater_modules_python/base.py
@@ -1,5 +1,6 @@
 """Stored base types shared by all sub-modules."""
 
+from re import S
 import xarray as xr
 from dataclasses import dataclass
 from typing import (
@@ -13,7 +14,7 @@ from typing import (
 )
 
 Process = Callable[..., float]
-ParametersDict = dict[str, float | int | bool]
+InitialVariablesDict = dict[str, float | int | bool]
 VariableTypes = Literal['static', 'dynamic', 'state']
 
 @dataclass(slots=True, frozen=True)
@@ -50,6 +51,33 @@ class CanRegisterVariable(Protocol):
 class Model(CanRegisterVariable):
     _variables: list[Variable] = []
     
+    def __init__(
+        self, 
+        initial_state_values: InitialVariablesDict,
+        static_variable_values: InitialVariablesDict,
+        track_dynamic_variables: bool = False,
+        time_dim: Optional[str] = None,
+    ) -> None:
+        """Initialize the model, should be accessed by subclasses.
+
+        Args:
+            initial_values: A dict with variable names as keys, and initial 
+                values for static and (optionally) state variables as values.
+        """
+        self.initial_state_values = initial_state_values
+        self.static_variable_values = static_variable_values
+        self.track_dynamic_variables = track_dynamic_variables
+        if not time_dim:
+            time_dim = 'time_step'
+        self.time_dim = time_dim
+
+        # initialize the main model dataset
+        self.dataset: xr.Dataset = self.init_state_arrays()
+
+        # make 2-dimensional static variable arrays
+        self.init_static_arrays()
+
+
     @classmethod
     def get_variable_names(cls) -> list[str]:
         """Return a list of default variable names."""
@@ -98,6 +126,58 @@ class Model(CanRegisterVariable):
         """Run the process."""
         ...
     
+    def init_state_arrays(self) -> xr.Dataset:
+        """Initializes the state arrays."""
+        ds: xr.Dataset = None
+
+        match_dims: list[str] = []
+        arrays: list[xr.DataArray] = []
+
+        for k, v in self.initial_state_values.items():
+            if k not in self.state_variables:
+                continue
+            if not isinstance(v, xr.DataArray):
+                match_dims.append(k)
+            else:
+                arrays.append(v)
+
+        # TODO: write this logic
+
+        for state_var in self.state_variables:
+            if state_var.name not in self.initial_state_values.keys():
+                 raise ValueError(
+                     f'No initial value found for state variable: {state_var.name}.'
+                 )
+
+    def init_static_arrays(self) -> None:
+        """Return a static dataset."""
+        ds: xr.Dataset = xr.Dataset(
+            coords=self.dataset.coords,
+            name='static_variables',
+        )
+        for var in self.static_variables:
+            if var.name not in self.static_variable_values.keys():
+                raise ValueError(
+                    f'No initial value found for static variable: {var.name}.'
+                )
+            if var.name in ds.coords.keys():
+                raise ValueError(
+                    f'Variable name {var.name} already exists in coords.'
+                )
+            attrs = {
+                'long_name': var.long_name,
+                'units': var.units,
+                'description': var.description,
+            }
+            ds[var.name] = xr.DataArray(
+                data=self.static_variable_values[var.name],
+                dims=ds.dims,
+                coords=ds.coords,
+                attrs=attrs,
+            )
+        return ds
+
+
 def register_variable(models: CanRegisterVariable | Iterable[CanRegisterVariable]):
     """A decorator to register a variable with a model."""
     if not isinstance(models, Iterable):
--- tests/test_3_model.py
+++ tests/test_3_model.py
@@ -5,15 +5,25 @@ import xarray as xr
 from clearwater_modules_python.base import (
     Model,
     Variable,
-    CanRegisterVariable,
+    InitialVariablesDict,
 )
 
 class MockModel(Model):
-    def __init__(self, initial_state: xr.DataArray):
-        self.initial_state = initial_state    
+    ...
 
 def state_process(dynamic_2: float) -> float:
     return dynamic_2 * 2
+
+@pytest.fixture(scope='module')
+def initial_static_values() -> InitialVariablesDict:
+    return {'a': 1.0, 'b': 2.0}
+
+
+@pytest.fixture(scope='module')
+def initial_state_values(
+    initial_array: xr.DataArray,
+) -> InitialVariablesDict:
+    return {'state_variable': initial_array}
 
 @pytest.fixture(scope='module')
 def state_variable() -> Variable:
@@ -26,24 +36,27 @@ def state_variable() -> Variable:
         process=state_process,
     )
 
+
 @pytest.fixture(scope='module')
 def model(
     static_variables: list[Variable],
     dynamic_variables: list[Variable],
     state_variable: Variable,
-    initial_array: xr.DataArray,
-) -> CanRegisterVariable:
-
-
+    initial_static_values: InitialVariablesDict,
+    initial_state_values: InitialVariablesDict,
+) -> Model:
     for var in static_variables + dynamic_variables:
         MockModel.register_variable(var)
     MockModel.register_variable(state_variable)
     
-    model_instance = MockModel(initial_state=initial_array)
+    model_instance = MockModel(
+        initial_state_values=initial_state_values,
+        static_variable_values=initial_static_values,
+    )
     return model_instance
 
 def test_model_instance(
-    model: CanRegisterVariable,
+    model: Model,
     static_variables: list[Variable],
     dynamic_variables: list[Variable],
 ) -> None:
@@ -58,3 +71,17 @@ def test_model_instance(
     assert len(model.dynamic_variables) == len(dynamic_variables)
     print(f'State variables: {model.state_variables}\n')
     assert len(model.state_variables) == 1
+
+def test_initial_state(model: Model, state_variable: Variable) -> None:
+    """Test the initial state."""
+    assert state_variable.name in model.dataset.data_vars
+    assert len(model.dataset[state_variable.name].dims) == 3 
+    assert 'time_step' in model.dataset[state_variable.name].dims
+    assert model.dataset[state_variable.name].shape == (1, 10, 10)
+
+def test_static_array(model: Model) -> None:
+    """Test the static array."""
+    for var in model.static_variables:
+        assert var.name in model.dataset.data_vars
+        assert len(model.dataset[var.name].dims) == 2
+        assert model.dataset[var.name].shape == (10, 10)
