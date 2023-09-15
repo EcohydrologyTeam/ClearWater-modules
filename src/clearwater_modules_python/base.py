1c0f2fbbff1db7cf15e0867f94d5562932c36f18
xaviernogueira <xavier.rojas.nogueira@gmail.com>
2023-09-15 12:20:29 -0400

    Improve init pattern for base

--- src/clearwater_modules_python/base.py
+++ src/clearwater_modules_python/base.py
@@ -28,49 +28,180 @@ class Model(CanRegisterVariable):
 
     def __init__(
         self,
-        initial_state_values: InitialVariablesDict,
-        static_variable_values: InitialVariablesDict,
+        initial_state_values: Optional[InitialVariablesDict] = None,
+        static_variable_values: Optional[InitialVariablesDict] = None,
         track_dynamic_variables: bool = True,
-        hotstart_dataset: xr.Dataset | None = None,
+        hotstart_dataset: Optional[xr.Dataset] = None,
         time_dim: Optional[str] = None,
     ) -> None:
         """Initialize the model, should be accessed by subclasses.
 
         Args:
-            initial_values: A dict with variable names as keys, and initial 
-                values for static and (optionally) state variables as values.
+            initial_state_values: A dict with variable names as keys, and initial 
+                state variables as values.
+            static_variable_values: A dict with variable names as keys, and static
+                values as values. All Model.static_variables must be present.
+                NOTE: This may be triggered in a subclass __init__ method.
+            track_dynamic_variables: If True, dynamic variables will be tracked
+                in the model dataset. If False, they will not be tracked.
+            hotstart_dataset: An optional dataset to use as a hotstart. 
+                This skips the initialization of the model dataset, and uses the 
+                provided dataset instead after validating the presence of all static 
+                and state variables.
+            time_dim: The name of the time dimension. If not provided, defaults
+                to 'time_step'.
         """
+        self.initial_state_values = initial_state_values
+        self.static_variable_values = static_variable_values
+        self.hotstart_dataset = hotstart_dataset
+        self.track_dynamic_variables = track_dynamic_variables
+
+        if not time_dim:
+            time_dim = 'time_step'
+        self.time_dim = time_dim
+       
+        if isinstance(self.initial_state_values, dict) and isinstance(self.static_variable_values, dict):
+            print('Initializing from dicts...')
+            self.dataset: xr.Dataset = self._init_dataset_from_dicts(
+                initial_state_values=self.initial_state_values,
+                static_variable_values=self.static_variable_values,
+            )
+
+        elif isinstance(hotstart_dataset, xr.Dataset):
+            print('Initializing from hotstart dataset...')
+            self.dataset: xr.Dataset = self._init_from_dataset(hotstart_dataset)
+            self.hotstart_dataset = None
+
+        else:
+            raise ValueError(
+                'Must provide either initial state and static values, or a hotstart dataset.'
+            )
+
+        self._sorted_variables: list[Variable] = []
+    
+    def _init_dataset_from_dicts(
+        self,
+        initial_state_values: InitialVariablesDict,
+        static_variable_values: InitialVariablesDict,
+    ) -> None:
+        """Initialize Model.dataset from dicts."""
         if not isinstance(initial_state_values, dict):
             raise TypeError(
-                f'Expected initial state value dict, got {type(initial_state_values)} instead.'
+                f'Expected initial state value dict, got {type(self.initial_state_values)} instead.'
             )
-        self.initial_state_values = initial_state_values
+        if not isinstance(static_variable_values, dict):
+            raise TypeError(
+                f'Expected static variable value dict, got {type(self.static_variable_values)} instead.'
+            )
         for state_var in self.state_variables:
-            if state_var.name not in self.initial_state_values.keys():
+            if state_var.name not in initial_state_values.keys():
                 raise ValueError(
                     f'No initial value found for state variable: {state_var.name}.'
                 )
 
-        self.static_variable_values = static_variable_values
-        self.track_dynamic_variables = track_dynamic_variables
-        if not time_dim:
-            time_dim = 'time_step'
-        self.time_dim = time_dim
+        # initialize the main model dataset
+        dataset: xr.Dataset = self._init_state_arrays(initial_state_values)
+        dataset: xr.Dataset = self._init_static_arrays(
+            dataset, 
+            static_variable_values,
+        )
 
-        if hotstart_dataset is not None:
-            if self.time_dim not in hotstart_dataset.dims:
-                raise ValueError(
-                    f'Hotstart dataset must have a {self.time_dim} dimension.'
+        print('Model initialized from input dicts successfully!.')
+        return dataset
+
+    def _init_from_dataset(self, hotstart_dataset: xr.Dataset) -> xr.Dataset:
+        """Initialize the model from a hotstart dataset."""
+        if self.time_dim not in hotstart_dataset.dims:
+            raise ValueError(
+                f'Hotstart dataset must have a {self.time_dim} dimension.'
+            )
+        return hotstart_dataset
+
+    def _init_state_arrays(
+        self,
+        initial_state_values: InitialVariablesDict,
+    ) -> xr.Dataset:
+        """Initializes the state arrays."""
+        match_dims: list[str] = []
+        data_arrays: dict[str, xr.DataArray] = {}
+
+        for k, v in initial_state_values.items():
+            if k not in self.state_variables_names:
+                warnings.warn(
+                    f'Variable {k} is not a state variable, skipping.',
                 )
-            self.dataset = hotstart_dataset
+                continue
+            if not isinstance(v, xr.DataArray):
+                match_dims.append(k)
+            else:
+                utils.validate_arrays(v, *list(data_arrays.values()))
+                data_arrays[k] = v
+        if len(data_arrays) > 0:
+            array_i = list(data_arrays.values())[0]
         else:
-            # initialize the main model dataset
-            self.dataset: xr.Dataset = self.init_state_arrays()
+            array_i = xr.DataArray(
+                [[1.0]],
+                dims=['x', 'y'],
+                coords=[[1.0], [1.0]],
+            )
+        for var_name in match_dims:
+            variable = self.get_variable(var_name)
+            attrs = {
+                'long_name': variable.long_name,
+                'units': variable.units,
+                'description': variable.description,
+            }
+            data_arrays[var_name] = xr.full_like(
+                array_i,
+                initial_state_values[var_name],
+                dtype=type(initial_state_values[var_name]),
+            )
+            data_arrays[var_name].attrs = attrs
+        ds = xr.Dataset(
+            data_vars=data_arrays,
+            coords=array_i.coords,
+        )
+        return ds.expand_dims({self.time_dim: [0]})
 
-            # make 2-dimensional static variable arrays
-            self.init_static_arrays()
+    def _init_static_arrays(
+        self, 
+        dataset: xr.Dataset,
+        static_variable_values: InitialVariablesDict,
+    ) -> xr.Dataset:
+        """Broadcasts static variables to an existing dataset.
+        
+        Args:
+            dataset: The dataset to broadcast to.
+            static_variable_values: A dictionary of static variable names and 
+                values (either float/bool/int or a xarray.DataArray).
 
-        self._sorted_variables: list[Variable] = []
+        Returns:
+            The dataset with the static variables added.
+        """
+        for var in self.static_variables:
+            if var.name not in static_variable_values.keys():
+                raise ValueError(
+                    f'No initial value found for static variable: {var.name}.'
+                )
+            if var.name in dataset.coords.keys():
+                raise ValueError(
+                    f'Variable name {var.name} already exists in coords.'
+                )
+            attrs = {
+                'long_name': var.long_name,
+                'units': var.units,
+                'description': var.description,
+            }
+            dataset[var.name] = xr.full_like(
+                dataset[
+                    list(dataset.data_vars)[0]
+                ].isel({self.time_dim: 0}),
+                static_variable_values[var.name],
+                dtype=type(static_variable_values[var.name]),
+            )
+            dataset[var.name].attrs = attrs
+        return dataset
+
 
     @classmethod
     def get_variable_names(cls) -> list[str]:
@@ -178,75 +309,6 @@ class Model(CanRegisterVariable):
         if not self.track_dynamic_variables:
             self.dataset = self.dataset.drop_vars(self.dynamic_variables_names)
         return self.dataset
-
-    def init_state_arrays(self) -> xr.Dataset:
-        """Initializes the state arrays."""
-        match_dims: list[str] = []
-        data_arrays: dict[str, xr.DataArray] = {}
-
-        for k, v in self.initial_state_values.items():
-            if k not in self.state_variables_names:
-                warnings.warn(
-                    f'Variable {k} is not a state variable, skipping.',
-                )
-                continue
-            if not isinstance(v, xr.DataArray):
-                match_dims.append(k)
-            else:
-                utils.validate_arrays(v, *list(data_arrays.values()))
-                data_arrays[k] = v
-        if len(data_arrays) > 0:
-            array_i = list(data_arrays.values())[0]
-        else:
-            array_i = xr.DataArray(
-                [[1.0]],
-                dims=['x', 'y'],
-                coords=[[1.0], [1.0]],
-            )
-        for var_name in match_dims:
-            variable = self.get_variable(var_name)
-            attrs = {
-                'long_name': variable.long_name,
-                'units': variable.units,
-                'description': variable.description,
-            }
-            data_arrays[var_name] = xr.full_like(
-                array_i,
-                self.initial_state_values[var_name],
-                dtype=type(self.initial_state_values[var_name]),
-            )
-            data_arrays[var_name].attrs = attrs
-        ds = xr.Dataset(
-            data_vars=data_arrays,
-            coords=array_i.coords,
-        )
-        return ds.expand_dims({self.time_dim: [0]})
-
-    def init_static_arrays(self) -> None:
-        """Return a static dataset."""
-        for var in self.static_variables:
-            if var.name not in self.static_variable_values.keys():
-                raise ValueError(
-                    f'No initial value found for static variable: {var.name}.'
-                )
-            if var.name in self.dataset.coords.keys():
-                raise ValueError(
-                    f'Variable name {var.name} already exists in coords.'
-                )
-            attrs = {
-                'long_name': var.long_name,
-                'units': var.units,
-                'description': var.description,
-            }
-            self.dataset[var.name] = xr.full_like(
-                self.dataset[
-                    list(self.dataset.data_vars)[0]
-                ].isel({self.time_dim: 0}),
-                self.static_variable_values[var.name],
-                dtype=type(self.static_variable_values[var.name]),
-            )
-            self.dataset[var.name].attrs = attrs
-
 
 def register_variable(models: CanRegisterVariable | Iterable[CanRegisterVariable]):
     """A decorator to register a variable with a model."""
