b43c84f63624b620a3e4c1ea72fdfb6a4bf11451
Isaac Mudge <66634423+imscw95@users.noreply.github.com>
2024-01-10 09:40:37 -0600

    Implemented TSM in notebook, started editing NSM model
    Beginning to try to implement nsm.

--- src/clearwater_modules/nsm1/model.py
+++ src/clearwater_modules/nsm1/model.py
@@ -20,14 +20,42 @@ class NutrientBudget(base.Model):
         self,
         initial_state_values: Optional[base.InitialVariablesDict] = None,
         algae_parameters: Optional[dict[str, float]] = None,
+        alkalinity_parameters: Optional[dict[str, float]] = None,
+        balgae_parameters: Optional[dict[str, float]] = None,
+        carbon_parameters: Optional[dict[str, float]] = None,
+        CBOD_parameters: Optional[dict[str, float]] = None,
+        DOX_parameters: Optional[dict[str, float]] = None,
+        nitrogen_parameters: Optional[dict[str, float]] = None,
+        POM_parameters: Optional[dict[str, float]] = None,           
         track_dynamic_variables: bool = True,
         hotstart_dataset: Optional[xr.Dataset] = None,
         time_dim: Optional[str] = None,
     ) -> None:
-        self.__algae_parameters: constants.Algae = constants.DEFAULT_Algae
+        self.__algae_parameters: constants.Algae = constants.DEFAULT_ALGAE
+        self.__alkalinity_parameters: constants.Alkalinity = constants.DEFAULT_ALKALINITY
+        self.__balgae_parameters: constants.Balgae = constants.DEFAULT_BALGAE
+        self.__carbon_parameters: constants.Carbon = constants.DEFAULT_CARBON
+        self.__CBOD_parameters: constants.CBOD = constants.DEFAULT_CBOD
+        self.__DOX_parameters: constants.DOX = constants.DEFAULT_DOX
+        self.__nitrogen_parameters: constants.DOX = constants.DEFAULT_DOX
+        self.__POM_parameters: constants.DOX = constants.DEFAULT_DOX
+        
 
         if algae_parameters is None:
-            algae_parameters_parameters = {}
+            algae_parameters = {}
+        if alkalinity_parameters is None:
+            alkalinity_parameters = {}
+        if balgae_parameters is None:
+            balgae_parameters = {}
+        if carbon_parameters is None:
+            carbon_parameters = {}
+        if CBOD_parameters is None:
+            CBOD_parameters = {}
+        if DOX_parameters is None:
+            DOX_parameters = {}
+
+
+
 
         # set default values
         for key, value in self.__algae_parameters.items():
