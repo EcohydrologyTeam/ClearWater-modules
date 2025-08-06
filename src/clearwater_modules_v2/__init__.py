9b3dc01ca58684acde4c8344ffa585c35e4c1f09
Anthony Aufdenkampe <aaufdenkampe@limno.com>
2025-08-06 15:10:55 -0500

    Update xarray example for input datasets

---
+++ src/clearwater_modules_v2/__init__.py
@@ -0,0 +1,5 @@
+"""Refactor of Clearwater modules"""
+
+# import clearwater_modules_v2.processes as processes
+import clearwater_modules_v2.utils as utils
+import clearwater_modules_v2.variables as variables
