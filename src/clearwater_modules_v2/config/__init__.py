c6d5098dc3526d9df2bb62dace08cc9e72316673
Paul Tomasula <ptomasula@gmail.com>
2025-09-12 13:58:05 -0400

    Start adding documentation
    Adds setup/install instructions, example configuration and more documentation, and start of TSM example.

--- src/clearwater_modules_v2/config/__init__.py
+++ src/clearwater_modules_v2/config/__init__.py
@@ -1,2 +1,3 @@
 from config.read import read_config
 from config.init import init_from_config, init_from_file
+from config.example import main as export_example
--- src/clearwater_modules_v2/config/example.py
+++ src/clearwater_modules_v2/config/example.py
@@ -1,31 +1,27 @@
 """Write out an example configuration file"""
 
-import yaml
 from pathlib import Path
-
-config = {
-    "model": {
-        "start_time": "2022-05-14 00:00:00",
-        "end_time": "2022-05-15 00:00:00",
-        "time_step": "30s",
-    },
-    "processes": [
-        {"riverine": {"configuration_path": "riverine_config.yml"}},
-        {"temperature": {"wind_a": 1.3, "wind_b": 1.5, "wind_c": 3.0}},
-    ],
-    "sources": {
-        "source_name_1": {
-            "provider": "csv",
-            "data": {"file_path": "path_to_csv"},
-        }
-    },
-}
+import sys
+import shutil
 
 
-def save_example_config(path: Path):
-    with open(path, "w") as f:
-        yaml.dump(config, f)
+def save_example_config(target_path: Path) -> None:
+    source_path = Path(__file__).parent / "example_config.yml"
+    shutil.copy(source_path, target_path)
+
+
+def main(*args) -> None:
+    # if no directory provided, use current directory
+    if len(args) < 2:
+        return save_example_config(Path.cwd() / "example_config.yml")
+    path = Path(args[1])
+    # if user provided a file, confirm that it is a yaml file
+    if path.suffix == ".yml" or path.suffix == ".yaml":
+        return save_example_config(path)
+    raise ValueError(
+        "Provided pathway must be a valid `.yaml` or `yml` file or a directory location."
+    )
 
 
 if __name__ == "__main__":
-    save_example_config(Path("example_config.yml"))
+    main(*sys.argv[1:])
